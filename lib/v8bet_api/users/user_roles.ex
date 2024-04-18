94b32a85e39effdf6dc16e00953cc1987d3f074b
breakage <dev.iosync@gmail.com>
2024-04-18 09:24:11 +0300

    Creates relationship between users and roles via user_roles

--- lib/v8bet_api/users/user_roles.ex
+++ lib/v8bet_api/users/user_roles.ex
@@ -2,7 +2,7 @@ defmodule V8betApi.Users.UserRoles do
   use Ecto.Schema
 
   schema "user_roles" do
-    belongs_to :user, V8betApi.User.User
+    belongs_to :user, V8betApi.Users.User
     belongs_to :role, V8betApi.Roles.Role
     timestamps(type: :utc_datetime)
   end
