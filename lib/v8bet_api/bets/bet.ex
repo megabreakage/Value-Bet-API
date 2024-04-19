fe1777622af5698269d124f240df19f0a88b876c
breakage <dev.iosync@gmail.com>
2024-04-19 04:54:22 +0300

    Adds estimated bet expiry_date

--- lib/v8bet_api/bets/bet.ex
+++ lib/v8bet_api/bets/bet.ex
@@ -6,14 +6,15 @@ defmodule V8betApi.Bets.Bet do
   @foreign_key_type :binary_id
   schema "bets" do
     field :amount, :float
-    field :expires_at, :utc_datetime
+    field :est_expires_at, :utc_datetime
+    field :expired_at, :utc_datetime
     field :start_time, :utc_datetime
     field :user_id, :binary_id
     field :match_id, :binary_id
     field :odd_id, :binary_id
     field :bet_status_id, :binary_id
-    field :added_by, :binary_id
     field :updated_by, :binary_id
+    field :deleted_at, :utc_datetime
 
     timestamps(type: :utc_datetime)
   end
@@ -21,7 +22,7 @@ defmodule V8betApi.Bets.Bet do
   @doc false
   def changeset(bet, attrs) do
     bet
-    |> cast(attrs, [:amount, :start_time, :expires_at])
-    |> validate_required([:amount, :start_time, :expires_at])
+    |> cast(attrs, [:user_id, :match_id, :odd_id, :bet_status_id, :amount, :start_time, :est_expires_at, :expired_at, :updated_by])
+    |> validate_required([:user_id, :match_id, :odd_id, :amount, :start_time, :est_expires_at])
   end
 end
--- priv/repo/migrations/20240419014251_create_bets.exs
+++ priv/repo/migrations/20240419014251_create_bets.exs
@@ -4,12 +4,14 @@ defmodule V8betApi.Repo.Migrations.CreateBets do
   def change do
     create table(:bets, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :amount, :float
-      add :start_time, :utc_datetime
-      add :expires_at, :utc_datetime
+
       add :user_id, references(:users, on_delete: :nothing, type: :binary_id)
       add :match_id, references(:matches, on_delete: :nothing, type: :binary_id)
       add :odd_id, references(:odds, on_delete: :nothing, type: :binary_id)
+      add :amount, :float
+      add :start_time, :utc_datetime
+      add :est_expires_at, :utc_datetime
+      add :expired_at, :utc_datetime
       add :bet_status_id, references(:bet_statuses, on_delete: :nothing, type: :binary_id)
       add :added_by, references(:users, on_delete: :nothing, type: :binary_id)
       add :updated_by, references(:users, on_delete: :nothing, type: :binary_id)
